fd41ffb610a16258940faad8da43a13eab28c446
Fany Louise <gandra.fany@gmail.com>
2024-09-10 17:37:23 -0300

    starting with react

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <canvas className="App">
+      <h1>Vite + React 1:12</h1>
+    </canvas>
+  )
+}
+export default App;
--- vite/main.js
+++ vite/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 const canvas = document.getElementById('canvas');
 
@@ -9,11 +10,11 @@ const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHe
 camera.position.z = 5;
 
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({color:'#468585',emissive:'#1a1a1a'});
+const material = new THREE.MeshLambertMaterial({color:'#f3d2b7', emissive:'#f3d2b7'});
 const dodecahedron = new THREE.Mesh(geometry, material);
 
-const boxGeometry = new THREE.BoxGeometry({width:2, height:0.1, depth:2});
-const boxMaterial = new THREE.MeshBasicMaterial({color:'#b4b4b4', emissive:'#1a1a1a'});
+const boxGeometry = new THREE.BoxGeometry(2,0.1,2);
+const boxMaterial = new THREE.MeshStandardMaterial({color:'#292f56', emissive:'#292f56'});
 const box = new THREE.Mesh(boxGeometry, boxMaterial)
 
 box.position.y = -1.5;
@@ -27,5 +28,30 @@ scene.add(light)
 
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true;
+controls.enableFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  dodecahedron.rotation.x += 0.01;
+
+  box.rotation.y += 0.005
+
+  controls.update()
+  renderer.render(scene,camera);
+}
+
+
+window.addEventListener('resize', ()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+animate()
